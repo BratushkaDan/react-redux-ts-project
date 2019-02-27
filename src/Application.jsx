fce4bd0800f5d813092b8a38554f54ae80c612e6
Sader <saderdan9@gmail.com>
2019-02-27 13:19:30 +0300

    Webpack pattern.

---
+++ src/Application.jsx
@@ -0,0 +1,17 @@
+// package imports
+import "core-js/es6/map";
+import "core-js/es6/set";
+import "./styles/styles.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+// User imports
+
+//
+class App extends React.Component {
+    render() {
+        return <></>
+    }
+}
+ReactDOM.render(<App/>, document.getElementById("root"))
